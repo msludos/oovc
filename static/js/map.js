d0de78e0f5bb6614db9aed3d695de31093b4d77d
piwer-m <120250862+msludos@users.noreply.github.com>
2022-12-17 20:39:44 +0300

    Update style

--- static/js/map.js
+++ static/js/map.js
@@ -36,7 +36,7 @@ function setMapJson(json, coutry) {
                 {icon: icon_f}).bindPopup(feature.properties.name);
             },
             onEachFeature: function (feature, latlng) {
-                if (feature.geometry.type != "Point") latlng.bindPopup(`<div class="popup-country"><img src="/static/imgs/countries/${coutry.id}.png"><hr><a href="/countries/country.html?id=${coutry.id}">${coutry.name}</a></div>`);
+                if (feature.geometry.type != "Point") latlng.bindPopup(`<div class="popup-country"><img src="/static/imgs/countries/${coutry.id}.png"><hr><a href="/countries/country.html?id=${coutry.id}">${coutry.name}❯</a></div>`);
             }
         }).addTo(map);
     });
