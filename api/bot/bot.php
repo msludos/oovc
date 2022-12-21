2405cce90d1dc2e2262b730097278b8c5776b534
piwer-m <120250862+msludos@users.noreply.github.com>
2022-12-21 23:22:40 +0300

    Update bot.php

--- api/bot/bot.php
+++ api/bot/bot.php
@@ -1,11 +1,11 @@
 <?php
-$token = "d98a7db1";
+$token = "vk1.a.bNGU0eqrn_O5mYDsEFxeeoK4JwznJ3axFjqcv_GqndH4elX3MRRhP9u3O-5Si9CEglYZ5tatPe146cxkw5VFvMVimWVDVtlV5WjbUFjGonc9Ly6fUCCZxveCIgZ8PqniF8RCvBtpov_vMuRI7ppcMWq6VgZT3pnzxHw-BkAQ1XJ0i1DnP5L2vIE6q-mfoWk05r73ryPXr8QNqmrP3EyfXA";
 
 function vk_msg_send($id, $text){
 	$request_params = array(
 		'message' => $text, 
 		'peer_id' => $id, 
-		'access_token' => $token,
+		'access_token' => "d98a7db1",
 		'v' => '5.87' 
 	);
 	$get_params = http_build_query($request_params); 
@@ -22,7 +22,7 @@ switch ($data->type) {
 	case 'message_new': 
 		$message_text = $data -> object -> text;
 		$chat_id = $data -> object -> peer_id;
-
+        
 		if ($message_text == "привет"){
 			vk_msg_send($chat_id, "Привет, я бот, который говорит две фразы.");
 		}
