4428863e898cf03e36e1035039f381cbb506296e
piwer-m <120250862+msludos@users.noreply.github.com>
2022-12-22 00:10:44 +0300

    Update ret.php

--- api/ret.php
+++ api/ret.php
@@ -1,3 +1,8 @@
 <?php
-    echo "hello".$_GET["name"];
+    $jsonurl = "https://api.vk.com/method/users.get?user_ids=".$_GET['id']."&access_token=vk1.a.5nwNgBGEcsAU__rTpV54q8Kq7eKbluzbq8v4Remw_--1I8bzXFLBL4NeCesWf-L1Rtvk6w73pjuMZmp1EtzSdy1OSXzxPi99AB1vGnGDdyVC7h_KW_HRd48nzwCHuoiEX1dBYMIkAdCA_xR9JaWaNAL9YIVCkha-9M75EHgroi316qrMeAMQgSHC3yMm0AQw&v=5.131";
+    $json = file_get_contents($jsonurl);
+    
+    $decode = json_decode($json, true);
+
+    echo "hello".$decode["response"][0]["first_name"];
 ?>
