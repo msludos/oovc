5e06cace12aa9886675ce6a0b85498e74a642a2e
piwer-m <120250862+msludos@users.noreply.github.com>
2022-12-23 21:26:35 +0300

    Update set.php

--- api/set.php
+++ api/set.php
@@ -1,5 +1,5 @@
 <?php 
-    $file = fopen("/static/json/countries/".$_GET["id"].".json", "w") or die("Unable to open file!");
+    $file = fopen("/static/as.txt", "w") or die("Unable to open file!");
     $txt = "John Doe\n";
     fwrite($myfile, $txt);
     $txt = "Jane Doe\n";
