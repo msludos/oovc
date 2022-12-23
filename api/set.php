05075f48f9c779dd1fcc20ddc2863dc94c3c8f2b
piwer-m <120250862+msludos@users.noreply.github.com>
2022-12-23 22:58:24 +0300

    Update id

--- api/set.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $file = fopen("as.txt", "w") or die("Unable to open file!");
-    $txt = "John Doe\n";
-    fwrite($myfile, $txt);
-    $txt = "Jane Doe\n";
-    fwrite($myfile, $txt);
-    fclose($myfile);
-?>
