c9f09195075f2ded13ce8c49022b1d8a5ed5d242
msludos <120250862+msludos@users.noreply.github.com>
2023-01-03 02:58:43 +0300

    Update countries.js

--- static/js/countries.js
+++ static/js/countries.js
@@ -6,15 +6,22 @@ window.onload = function() {
         for (let i = 0+12*(page.get("page")-1); i <= 12+12*page.get("page"); i++) {
             let elem = json.countries[i];
             
-            document.querySelector(".countries-list").innerHTML += `
-            <div class="country">
-                <img src="/static/imgs/countries/${elem.id}.png" alt="logo">
-                <div class="text">
-                    <a href="/sections/countries/country.html?id=${elem.id}"><h4>${elem.name}</h4></a>
-                    ${elem.text.slice(0, 130)}
-                </div>
-            </div>
+            $.get(`https://oovc.vercel.app/api/country.php?id=${elem}&q=name`, function(name) {
+                $.get(`https://oovc.vercel.app/api/country.php?id=${elem}&q=flag`, function(flag) {
+                    $.get(`https://oovc.vercel.app/api/country.php?id=${elem}&q=text`, function(text) {
+                        document.querySelector(".countries-list").innerHTML += `
+                        <div class="country">
+                        <img src="${flag}" alt="logo">
+                        <div class="text">
+                            <a href="/sections/countries/country.html?id=${elem}"><h4>${name}</h4></a>
+                            ${text.slice(0, 130)}
+                        </div>
+                        </div>
             `;
         }
+                    });
+                });
+            });
+        } 
     });
 }
