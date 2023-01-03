9c99510d1a6f606d72dcc9206a6f2d51ecef9d5f
msludos <120250862+msludos@users.noreply.github.com>
2023-01-03 03:10:09 +0300

    Update countries.js

--- static/js/countries.js
+++ static/js/countries.js
@@ -1,32 +1,3 @@
 window.onload = function() {
     alert("cr") ;
-    try {
-    fetch('/static/json/countries.json').then((response) => response.json())
-    .then((json) => {
-        let page = new URLSearchParams(location.search);
-
-        for (let i = 0+12*(page.get("page")-1); i <= 12+12*page.get("page"); i++) {
-            let elem = json.countries[i];
-            alert(elem);
-            $.get(`https://oovc.vercel.app/api/country.php?id=${elem}&q=name`, function(name) {
-                $.get(`https://oovc.vercel.app/api/country.php?id=${elem}&q=flag`, function(flag) {
-                    $.get(`https://oovc.vercel.app/api/country.php?id=${elem}&q=text`, function(text) {
-                        document.querySelector(".countries-list").innerHTML += `
-                        <div class="country">
-                        <img src="${flag}" alt="logo">
-                        <div class="text">
-                            <a href="/sections/countries/country.html?id=${elem}"><h4>${name}</h4></a>
-                            ${text.slice(0, 130)}
-                        </div>
-                        </div>
-            `;
-        }
-                    });
-                });
-            });
-        } 
-    });
-   } catch (e) {
-      alert(e);
-   } 
 }
