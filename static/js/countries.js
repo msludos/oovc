2615d699c70494071bfae9baeb58a18c339e3ad5
msludos <120250862+msludos@users.noreply.github.com>
2023-01-03 03:18:46 +0300

    Update countries.js

--- static/js/countries.js
+++ static/js/countries.js
@@ -1,3 +1,9 @@
 window.onload = function() {
-    alert("cr") ;
+    alert("cr");
+    fetch('/static/json/countries.json').then((response) => response.json())
+        .then((json) => {
+            json.countries.forEach(element => {
+                alert($.get(`https://oovc.vercel.app/api/country.php?id=${element}&q=name`));
+            });
+        } 
 }
