4d9aedf538f34fe9d798d4784575abcf173223f1
msludos <120250862+msludos@users.noreply.github.com>
2023-01-08 04:13:20 +0300

    Update countries.js

--- static/js/countries.js
+++ static/js/countries.js
@@ -2,15 +2,9 @@ window.onload = function() {
     try {
     fetch('/api/allcountries.php').then((response) => response.text())
         .then((txt) => {
-            let jsons = txt.split("~");
+            
             alert(txt);
-            jsons.forEach(element => {
-                if (element == "") continue;
-                let json = JSON.parse(element);
-                alert(element);
-                alert(json.name);
-                document.querySelector(".countries-list").innerHTML += json.name;
-            });
+            
     });
    } catch (e) { alert(e);}
 }
