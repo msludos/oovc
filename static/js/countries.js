a70e4b517933cc671d80d6f015e30cb2656d17b0
msludos <120250862+msludos@users.noreply.github.com>
2023-01-08 04:06:37 +0300

    Update countries.js

--- static/js/countries.js
+++ static/js/countries.js
@@ -2,6 +2,7 @@ window.onload = function() {
     fetch('/api/allcountries.php').then((response) => response.text())
         .then((txt) => {
             let jsons = txt.split("~");
+            alert(txt);
             jsons.forEach(element => {
                 if (element == "") continue;
                 let json = JSON.parse(element);
