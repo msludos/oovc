8b5bdaa9da19b2f7a5bab9e86212f1a3955ffc48
msludos <120250862+msludos@users.noreply.github.com>
2023-01-15 01:34:54 +0300

    Update country.php

--- api/country.php
+++ api/country.php
@@ -2,11 +2,5 @@
     $jsonurl = "https://OOVCbot.msludos.repl.co/countries?id=".$_GET["id"];
     $json = file_get_contents($jsonurl);
     
-    $decode = json_decode($json, true);
-    
-    if (("" === $decode[$_GET["q"]]) || (null === $decode[$_GET["q"]])) {
-        echo "undefined";
-    } else {
-        echo $decode[$_GET["q"]];
-    }
+    echo $json;
 ?>
