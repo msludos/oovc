bfba85b0b8c2cddecda56879311ccbc7a628156a
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-20 01:37:15 +0300

    Update register.js

--- static/js/register.js
+++ static/js/register.js
@@ -3,5 +3,4 @@ const urlParams = new URLSearchParams(queryString);
 
 window.onload = function() {
     document.querySelector("#id").value = urlParams.get("id");
-    document.querySelector("#submit").addEventListener('click', function(){document.querySelector("#id").removeAttribute("disabled")});
 }
