cc619bb3fc8c4116f615f490e1fbb58774862571
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-20 00:47:29 +0300

    Update register.php

--- api/register.php
+++ api/register.php
@@ -1,13 +1,12 @@
 <?php
     foreach ($_POST as &$value) {
         if (is_array($value)) {
-            echo "\n\n";
             foreach ($value as &$valuew) {
                 echo $valuew;
             }
-            echo "\n\n";
         } else {
             echo $value;
         }
+        echo "<br><br>"
     }
 ?>
