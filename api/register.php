5d6cd694ab210572c865d4050fa492a637ce1c68
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-22 00:32:02 +0300

    Update register.php

--- api/register.php
+++ api/register.php
@@ -9,7 +9,7 @@
         }
         echo "<br><br>";
     }
-    $json = '{"id": "'.$_POST["id"].'","name": "'.$_POST["name"].'","flag": "'.$_POST["flag"].'","date": "'.$_POST["date"].'","text": "'.$_POST["text"].'","vk": "'.$_POST["vk"].'","site": "'.$_POST["site"].'","valute-name": "'.$_POST["valute-name"].'","valute-change": "'.$_POST["valute-change"].'","form-government": "'.$_POST["form"][0].'","form-government-system": "'.$_POST["gover"][0].'","form-politic": "'.$_POST["politic"][0].'","status": "}
+    $json = '{"id": "'.$_POST["id"].'","name": "'.$_POST["name"].'","flag": "'.$_POST["flag"].'","date": "'.$_POST["date"].'","text": "'.$_POST["text"].'","vk": "'.$_POST["vk"].'","site": "'.$_POST["site"].'","valute-name": "'.$_POST["valute-name"].'","valute-change": "'.$_POST["valute-change"].'","form-government": "'.$_POST["forms"][0].'","form-government-system": "'.$_POST["gover"][0].'","form-politic": "'.$_POST["politic"][0].'","status": "}
     ".$_POST[""]."';
     echo "https://oovcbot.msludos.repl.co/data?key=".$_POST["key"]."&id=".$_POST["id"]."&text=".$json;
 ?>                                          
