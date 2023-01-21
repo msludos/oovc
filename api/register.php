7463a98f08f715da99986dca3e3c206a0a780cc0
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-21 23:51:35 +0300

    Update register.php

--- api/register.php
+++ api/register.php
@@ -9,4 +9,6 @@
         }
         echo "<br><br>";
     }
+
+    echo "https://oovcbot.msludos.repl.co/data?code=".$_POST["code"]."&name=".$_POST["name"]."&flag=".$_POST["flag"]."&date=".$_POST["date"]."&text=".$_POST["text"]."&vk=".$_POST["vk"]."&site=".$_POST["site"]."&valutename=".$_POST["valute-name"]."&valutechange=".$_POST["valute-change"]."&form=".$_POST["forms"]."&gover=".$_POST["gover"]."&politic=".$_POST["politic"]."&key=".$_POST["key"]."&id=".$_POST["id"];
 ?>
