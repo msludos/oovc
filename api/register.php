b1dc81e21fa0d7851db8db8715d7e17495a5dba0
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-22 18:16:26 +0300

    Update register.php

--- api/register.php
+++ api/register.php
@@ -9,7 +9,7 @@
         }
         echo "<br><br>";
     }
-    $json = '&id='.$_POST["id"].'&name'.$_POST["name"].'&flag='.$_POST["flag"].'&date='.$_POST["date"].'&text='.$_POST["text"].'&vk'.$_POST["vk"].'&site='.$_POST["site"].'&valute-name='.$_POST["valute-name"].'&valute-change='.$_POST["valute-change"].'&form-government='.$_POST["forms"][0].'&form-government-system='.$_POST["gover"][0].'&form-politic='.$_POST["politic"][0].'&status=0';
+    $json = '&id='.$_POST["id"].'&name='.$_POST["name"].'&flag='.$_POST["flag"].'&date='.$_POST["date"].'&text='.$_POST["text"].'&vk='.$_POST["vk"].'&site='.$_POST["site"].'&valute-name='.$_POST["valute-name"].'&valute-change='.$_POST["valute-change"].'&form-government='.$_POST["forms"][0].'&form-government-system='.$_POST["gover"][0].'&form-politic='.$_POST["politic"][0].'&status=0';
     $url = "https://oovcbot.msludos.repl.co/data?key=".$_POST["key"]."&id=".$_POST["id"].$json;
     $p = file_get_contents($url);
 ?>                               
