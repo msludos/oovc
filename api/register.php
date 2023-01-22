bb554a4f74d80249a4b9dcd92a3d7693af04f0c1
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-22 18:38:27 +0300

    Update register.php

--- api/register.php
+++ api/register.php
@@ -9,7 +9,31 @@
         }
         echo "<br><br>";
     }
-    $json = '&id='.$_POST["id"].'&name='.$_POST["name"].'&flag='.$_POST["flag"].'&date='.$_POST["date"].'&text='.$_POST["text"].'&vk='.$_POST["vk"].'&site='.$_POST["site"].'&valute-name='.$_POST["valute-name"].'&valute-change='.$_POST["valute-change"].'&form-government='.$_POST["forms"][0].'&form-government-system='.$_POST["gover"][0].'&form-politic='.$_POST["politic"][0].'&status=0';
-    $url = "https://oovcbot.msludos.repl.co/data?key=".$_POST["key"]."&id=".$_POST["id"].$json;
-    $p = file_get_contents($url);
+    $postdata = http_build_query(
+        array(
+            'key' => $_POST["key"],
+            'id' => $_POST["id"],
+            'name' => $_POST["name"],
+            'flag' => $_POST["flag"],
+            'date' => $_POST["date"],
+            'text' => $_POST["text"],
+            'vk' => $_POST["vk"],
+            'site' => $_POST["site"],
+            'valute-name' => $_POST["valute-name"],
+            'valute-change' => $_POST["valute-change"],
+            'form-government' => $_POST["forms"][0],
+            'form-government-system' => $_POST["gover"][0],
+            'form-politic' => $_POST["politic"][0],
+            '' => ''
+        )
+    );
+    $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+    $context  = stream_context_create($opts);
+    $result = file_get_contents('https://oovcbot.msludos.repl.co/data', false, $context);
 ?>                               
