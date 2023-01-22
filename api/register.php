6f9fedd8ea629cb370e5f7d3abd4b5131a510e00
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-22 18:01:58 +0300

    Update register.php

--- api/register.php
+++ api/register.php
@@ -11,6 +11,5 @@
     }
     $json = '{"id": "'.$_POST["id"].'","name": "'.$_POST["name"].'","flag": "'.$_POST["flag"].'","date": "'.$_POST["date"].'","text": "'.$_POST["text"].'","vk": "'.$_POST["vk"].'","site": "'.$_POST["site"].'","valute-name": "'.$_POST["valute-name"].'","valute-change": "'.$_POST["valute-change"].'","form-government": "'.$_POST["forms"][0].'","form-government-system": "'.$_POST["gover"][0].'","form-politic": "'.$_POST["politic"][0].'","status": ""}';
     $url = "https://oovcbot.msludos.repl.co/data?key=".$_POST["key"]."&id=".$_POST["id"]."&text=".$json;
-    $p = fopen($url, 'r');
-    close($p);
+    $p = file_get_contents($url);
 ?>                               
