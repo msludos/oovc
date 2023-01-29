54f47ca65d23f1fb9b77c8e9ca3e2fb6f1af68dc
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-29 17:26:36 +0300

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -61,11 +61,15 @@ fetch('https://oovc.vercel.app/api/countriesid.php').then((response) => response
     .then((ids) => {
         ids.split("~").forEach(element => {
             console.log(element);
-	        $.get(`https://oovc.vercel.app/api/country.php?id=${element}&fields=name,flag`, function(data) {
-		        let datas = data.split("~");
-		        fetch(`/static/json/geo/${element}.geojson`).then((response) => response.json())
-                            .then((json) => setMapJson(json, element, datas[1], datas[0])); 
-            });
+            try {
+                $.get(`https://oovc.vercel.app/api/country.php?id=${element}&fields=name,flag`, function(data) {
+                    let datas = data.split("~");
+                    fetch(`/static/json/geo/${element}.geojson`).then((response) => response.json())
+                                .then((json) => setMapJson(json, element, datas[1], datas[0])); 
+                });
+            } catch {
+                console.error(element+" load failed.");
+            }
         });
     });
 document.querySelector(".leaflet-attribution-flag").remove();
