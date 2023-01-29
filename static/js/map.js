43c86c1be4a535cd760be7e746ce199b97774739
msludos <120250862+msludos@users.noreply.github.com>
2023-01-29 17:10:56 +0300

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -63,7 +63,7 @@ fetch('https://oovc.vercel.app/api/countriesid.php').then((response) => response
 	    $.get(`https://oovc.vercel.app/api/country.php?id=${element}&fields=name,flag`, function(data) {
 		if (data == "deleted") return;
 		let datas = data.split("~");
-		fetch(`/static/json/geo/${element}.geojson`).then((response) => {console.log(element);return response.text();})
+		fetch(`/static/json/geo/${element}.geojson`).then((response) => {console.log(element);return response.json();})
                     .then((json) => setMapJson(json, element, datas[1], datas[0])); 
             });
         });
