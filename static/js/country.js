8c0b3f35ac24274e1a9bcd00beeb4edf19396e63
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-31 19:59:08 +0300

    Update country.js

--- static/js/country.js
+++ static/js/country.js
@@ -1,25 +1,17 @@
 window.onload = function() {
-    fetch('https://oovc.vercel.app/api/countriesid.php').then((response) => response.json())
-        .then((json) => {
-            console.log(json.list);
-            json.list.forEach(country => {
-                if (country == new URLSearchParams(location.search).get("id")) {
-                    $.get(`https://oovc.vercel.app/api/country.php?id=${country}&fields=name,flag,date,text,vk,author,site`, function(data) {
-                        let datas = data.split("~");
-                        
-                        console.log(datas);
-                        
-                        document.querySelector("title").innerText = datas[0];
-                        
-                        document.querySelector(".aside img").setAttribute("src", datas[1]);
-                        document.querySelector(".date").innerHTML = datas[2];
-                        document.querySelector(".description").innerHTML = datas[3].replace("~", "");
-                        
-                        document.querySelector("list a:first-child").setAttribute("href", datas[5]);
-                        if (datas[4] != "https://vk.com/"+country) document.querySelector(".info:nth-child(2)").setAttribute("href", datas[4]);
-                        if (datas[6] != "https://example.com") document.querySelector(".info:last-child").setAttribute("href", datas[6]);
-                    });
-                }
-        });
+    $.get(`https://oovc.vercel.app/api/country.php?id=${country}&fields=name,flag,date,text,vk,site,valute-name,valute-change,form-government,form-government-system,form-politic,status`, function(data) {
+        let datas = data.split("~");
+        
+        console.log(datas);
+        
+        document.querySelector("title").innerText = datas[0];
+        
+        document.querySelector(".aside img").setAttribute("src", datas[1]);
+        document.querySelector(".date").innerHTML = datas[2];
+        document.querySelector(".description").innerHTML = datas[3].replace("~", "");
+        
+        document.querySelector("list a:first-child").setAttribute("href", datas[5]);
+        if (datas[4] != "https://vk.com/"+country) document.querySelector(".info:nth-child(2)").setAttribute("href", datas[4]);
+        if (datas[6] != "https://example.com") document.querySelector(".info:last-child").setAttribute("href", datas[6]);
     });
 }
