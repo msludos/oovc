59c9c244fd181a6091f2ad9f94645a63074fc371
piwer-m <120250862+msludos@users.noreply.github.com>
2023-01-31 20:32:53 +0300

    Update country.html

--- static/js/country.js
+++ static/js/country.js
@@ -1,17 +1,39 @@
+function get(selector) {
+    return document.querySelector(selector);
+}
+
+function goverment(goverment_a, goverment_system, politic) {
+    switch (goverment_system) {
+        case "Унитарное": goverment_system = "Унитарная"; break;
+        case "Федеративное": goverment_system = "Федеративная"; break;
+    }
+    switch(politic) {
+        case "Тоталитарный": politic = "Тоталитарным"; break;
+        case "Авторитарный": politic = "Авторитарным"; break;
+        case "Демократический": politic = "Демократическим"; break;
+    }
+    return goverment_system+" "+goverment_a+" с "+politic+" режимом"
+}
+
 window.onload = function() {
     $.get(`https://oovc.vercel.app/api/country.php?id=${new URLSearchParams(location.search).get("id")}&fields=name,flag,date,text,vk,site,valute-name,valute-change,form-government,form-government-system,form-politic`, function(data) {
         let datas = data.split("~");
         
         console.log(datas);
         
-        document.querySelector("title").innerText = datas[0];
+        get("title").innerText = datas[0];
         
-        document.querySelector(".aside img").setAttribute("src", datas[1]);
-        document.querySelector(".date").innerHTML = datas[2];
-        document.querySelector(".description").innerHTML = datas[3].replace("~", "");
-        
-        document.querySelector("list a:first-child").setAttribute("href", datas[5]);
-        if (datas[4] != "https://vk.com/"+country) document.querySelector(".info:nth-child(2)").setAttribute("href", datas[4]);
-        if (datas[6] != "https://example.com") document.querySelector(".info:last-child").setAttribute("href", datas[6]);
+        get(".description h1").innerText = data[0];
+        get(".deccription").innerText += data[3];
+
+        get(".about img").setAttribute("src", data[1]);
+        get(".about .links .author").setAttribute("href", "https://vk.com/id"+new URLSearchParams(location.search).get("id"));
+        get(".about .links .vk").setAttribute("href", data[4]);
+        get(".about .links .site").setAttribute("href", data[5]);
+
+        get(".valute .name").innerText += data[6];
+        get(".valute .change").innerText += data[7];
+
+        get(".goverment").innerText = goverment(data[8], data[9], data[10]);
     });
 }
